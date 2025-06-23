b1b016a61bf79586d0f52686f0a1f347d7c13452
schen19258 <164255756+schen19258@users.noreply.github.com>
2025-06-22 23:57:59 -0500

    Add files via upload

---
+++ Saying-Hi-Starter/script.js
@@ -0,0 +1,11 @@
+console.log(STUDENTS);
+
+for (let i = 0; i < STUDENTS.length; i++) {
+  console.log("Hi " + STUDENTS[i] + "!!!!!!!!!!!!!!")
+}
+
+let i = 0;
+while (i < STUDENTS.length) {
+  console.log("Hi " + STUDENTS[i] + "!!!!!!!!!!!!!!")
+  i++;
+}
